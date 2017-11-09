f951fb3b493c362d8b51d348d3ae5f24b4d7a372
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-11-09 13:17:48 -0500

    asf

--- ApJavaClasswork/src/Lab31/ArrayMethods.java
+++ ApJavaClasswork/src/Lab31/ArrayMethods.java
@@ -1,21 +1,40 @@
 package Lab31;
 
 public class ArrayMethods {
+	public static void main(String[] args)
+	{
+		int[] TimZilong = {1,2,3,4,5,6,7,4,8};
+		System.out.print(toString(removeDuplicates(TimZilong)));
+	}
+	
 	public static int[] removeDuplicates(int[] list)
 	{
-		int[] noDupes = copyArray(list);
+		int dupes = 0;
+		for (int i = 0; i < list.length-1; i++)
+		{
+			for (int t = i+1; t < list.length; t++)
+			{
+				if (list[i] == list[t])
+				{
+					dupes++;
+				}
+			}
+		}
+		int[] noDupes = new int[list.length-dupes];
 		for (int i = 0; i < list.length-1; i++)
 		{
 			for (int t = i+1; t < list.length; t++)
 			{
 				if (list[i] != list[t])
 				{
-					
+					noDupes[t] = list[i];
 				}
 			}
 		}
+		return noDupes;
 	}
-	public static int[] copyArray(int[] intArray)
+	/*
+	 * public static int[] copyArray(int[] intArray)
 	{
 		int[] copy = new int[intArray.length];
 		
@@ -25,17 +44,32 @@ public class ArrayMethods {
 		}
 		return copy;	
 	}
+	*/
 	
 	
-	public static int[][] productArray(int[] arr1, int[] arr2)
+	//public static int[][] productArray(int[] arr1, int[] arr2)
 	{
 		
 	}
 	
-	public static int[][]  pascalTriangle(int n)
+	//public static int[][]  pascalTriangle(int n)
 	{
 		
 	}
 	
-	public static void printPascalTriangle(int[][] triangle)
+	//public static void printPascalTriangle(int[][] triangle)
+	{
+		
+	}
+	
+	public static String toString(int[] list)
+	{
+		String temp = "";
+		for (int i = 0; i < list.length; i++)
+		{
+			temp += list[i] + ", ";
+		}
+		return temp;
+			
+	}
 }
