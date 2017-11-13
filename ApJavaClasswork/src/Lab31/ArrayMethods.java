ad07d14696f101a886125c22e7d6833036af0f24
Tim <Tim@DESKTOP-O9RF4TE.fios-router.home>
2017-11-12 20:08:27 -0500

    ay lmao is tim

--- ApJavaClasswork/src/Lab31/ArrayMethods.java
+++ ApJavaClasswork/src/Lab31/ArrayMethods.java
@@ -3,8 +3,23 @@ package Lab31;
 public class ArrayMethods {
 	public static void main(String[] args)
 	{
-		int[] TimZilong = {1,2,3,4,5,6,7,4,8};
-		System.out.print(toString(removeDuplicates(TimZilong)));
+		int[] testT = {1,2,3,4,5,6,7,4,8,2,9,10,10};
+		System.out.print(toString(removeDuplicates(testT)));
+		System.out.println();
+		System.out.println();
+		
+		int[] testMulti1 = {5,4,5};
+		int[] testMulti2 = {2,3,2};
+		int[][] product = productArray(testMulti1, testMulti2);
+		for(int i = 0; i < testMulti1.length; i++)
+		{
+			for(int j = 0; j < testMulti2.length; j++)
+			{
+				System.out.print(" [ " + product[i][j] + " ] ");
+			}
+			System.out.println();
+		}
+		
 	}
 	
 	public static int[] removeDuplicates(int[] list)
@@ -17,39 +32,43 @@ public class ArrayMethods {
 				if (list[i] == list[t])
 				{
 					dupes++;
+					list[i] = list[0];
 				}
 			}
 		}
 		int[] noDupes = new int[list.length-dupes];
-		for (int i = 0; i < list.length-1; i++)
+		noDupes[0] = list[0];
+		int x = 1;
+		int y = 1;
+		while (x < noDupes.length)
 		{
-			for (int t = i+1; t < noDupes.length; t++)
+			if (list[y] != noDupes[0])
 			{
-				if (list[i] != list[t])
-				{
-					noDupes[i] = list[i];
-				}
+				noDupes[x] = list[y];
+				x++;
+				y++;
+			}
+			else
+			{
+				y++;
 			}
 		}
 		return noDupes;
 	}
-	/*
-	 * public static int[] copyArray(int[] intArray)
+	
+	public static int[][] productArray(int[] arr1,int[] arr2)
 	{
-		int[] copy = new int[intArray.length];
-		
-		for(int i =0;i < intArray.length;i++)
+		int[][] result = new int[arr1.length][arr2.length];
+		for(int i = 0; i < arr1.length; i++)
 		{
-			copy[i] = intArray[i];
+			int[] loop = new int[arr1.length];
+			for(int j = 0; j < arr1.length; j++)
+			{
+				loop[j] = arr1[i]*arr2[j];
+			}
+			result[i] = loop;
 		}
-		return copy;	
-	}
-	*/
-	
-	
-	//public static int[][] productArray(int[] arr1, int[] arr2)
-	{
-		
+		return result;
 	}
 	
 	//public static int[][]  pascalTriangle(int n)
@@ -67,9 +86,9 @@ public class ArrayMethods {
 		String temp = " ";
 		for (int i = 0; i < list.length; i++)
 		{
-			temp += list[i] + ", ";
+			temp += " [ " + list[i] + " ] ";
 		}
-		return temp;
-			
+		return temp;	
 	}
+	
 }
