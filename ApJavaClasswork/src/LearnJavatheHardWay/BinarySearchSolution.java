5bc94a44e5658cea47ba81b36427526e7ee9b32a
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-11-15 12:42:09 -0500

    as

---
+++ ApJavaClasswork/src/LearnJavatheHardWay/BinarySearchSolution.java
@@ -0,0 +1,44 @@
+package LearnJavatheHardWay;
+
+public class BinarySearchSolution {
+	
+	public static void main(String[] args)
+	{
+		int[] test1 = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+		int[] test2 = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+		
+		for(int i = -1; i < test1.length+1;i++)
+		{
+			System.out.print(binarySearch(test1, i , 0, test1.length) + " ");
+		}
+		System.out.println();
+		for(int i = -1; i < test2.length+1;i++)
+		{
+			System.out.print(binarySearch(test2, i , 0, test2.length) + " ");
+		}
+		System.out.println();
+	}
+	
+	public static int binarySearch(int[] nums, int query, int first, int last)
+	{
+		if(last > first) //if still have some array left to search
+		{
+			int index = (first+last)/2;
+			int guess = nums[index];
+			
+			if(guess == query)
+			{
+				return index;
+			}
+			
+			if(guess > query)
+			{
+				return binarySearch(nums, query, first, index);
+			}
+			
+			return binarySearch(nums, query, index + 1, last);
+		}
+		
+		return -1;
+	}
+}
