000f90ce195d1a82094dc231714b272f16e612f7
BT_1N3_21 <BT_1N3_21@BT_1N3_21.bktech.local>
2017-12-20 13:16:37 -0500

    asd

---
+++ ApJavaClasswork/src/Lab42/CSVUtilities.java
@@ -0,0 +1,35 @@
+package Lab42;
+
+import java.io.BufferedReader;
+import java.nio.file.Paths;
+
+public class CSVUtilities {
+	ArrayList<String> CSVData = new ArrayList<String>();
+	int numColumns;
+	
+	public CSVUtilities(File csv)
+	{
+		ArrayList<String> CSVData = new ArrayList<String>();
+		Path pathtofile = Paths.get("")
+	}
+	
+	public List<String> getColumnHeaders()
+	{
+		
+	}
+	
+	public List<String> getDataString(int column)
+	{
+		
+	}
+	
+	public List<Integer> getDataInt(int column)
+	{
+		
+	}
+	
+	public List<Integer> getDataDouble(int column)
+	{
+		
+	}
+}
