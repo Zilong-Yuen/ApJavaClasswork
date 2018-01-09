f8261af85911ad7028930d7b4313cf6af29380b1
BT_1N3_22 <BT_1N3_22@BT_1N3_22.bktech.local>
2018-01-09 12:47:05 -0500

    reee doesnt work

--- ApJavaClasswork/src/Lab43/GameCode.java
+++ ApJavaClasswork/src/Lab43/GameCode.java
@@ -1,6 +1,11 @@
 package Lab43;
 
 import javafx.application.Application;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Timer;
 import javafx.animation.AnimationTimer;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -9,34 +14,43 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class GameCode extends Application{
 	int counts = 0;
 	public void start(Stage primaryStage) throws Exception {
         primaryStage.setTitle("HBox Experiment 1");
-
+        
+        Text text1 = new Text();
         Button button = new Button("Click me!");
         
+        //long timeStep = System.nanoTime() + 1000000000;
+        
         button.setOnAction(value ->  {
+        	long timeStep = System.nanoTime() + 1000000000L;
         	counts++;
             System.out.println(counts);
+        	
+            if (System.nanoTime() > timeStep)
+            {
+            	System.out.println("It has been 10 seconds and you have clicked " + counts + " times");
+            }
          });
         
-        long timeStep = System.nanoTime() + 1000000000L;
-        new AnimationTimer()
+        
+        PrintWriter pw = null;
+        try 
         {
-
-			@Override
-			public void handle(long now) {
-				if (now > timeStep)
-				{
-					timeStep = now + 1000000000L;
-					
-				}
-			}
-        	
-        }.start();
+        	pw = new PrintWriter(new File("results.csv"));
+        }
+        catch(FileNotFoundException e)
+        {
+        	System.err.println(e);
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append("Name, High Score\n");
+        sb.append("Tim,counts\n");
         
         StackPane root = new StackPane();
         root.getChildren().add(button);
